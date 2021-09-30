5a71c90b0f94a3ff1927100628876c206e31aa76
Anton Khoda <Antonhoda98@gmail.com>
2021-09-30 14:42:32 +0300

    v1

---
+++ assets/plugins/dropdown/dropdown.js
@@ -0,0 +1,39 @@
+$('#dropdown_batton_1').click(function() {
+  if ( $( "#dropdown_content_1" ).is( ":hidden" ) ) {
+      $( "#dropdown_content_1" ).show( "slow" );
+      $("#dropdown_batton_1").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  } else {
+  $( "#dropdown_content_1" ).fadeOut("slow");
+  $("#dropdown_batton_1").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  }
+});
+
+$('#dropdown_batton_2').click(function() {
+  if ( $( "#dropdown_content_2" ).is( ":hidden" ) ) {
+      $( "#dropdown_content_2" ).show( "slow" );
+      $("#dropdown_batton_2").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  } else {
+  $( "#dropdown_content_2" ).fadeOut("slow");
+  $("#dropdown_batton_2").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  }
+});
+
+$('#dropdown_batton_3').click(function() {
+  if ( $( "#dropdown_content_3" ).is( ":hidden" ) ) {
+      $( "#dropdown_content_3" ).show( "slow" );
+      $("#dropdown_batton_3").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  } else {
+  $( "#dropdown_content_3" ).fadeOut("slow");
+  $("#dropdown_batton_3").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  }
+});
+
+$('#dropdown_batton_4').click(function() {
+  if ( $( "#dropdown_content_4" ).is( ":hidden" ) ) {
+      $( "#dropdown_content_4" ).show( "slow" );
+      $("#dropdown_batton_4").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  } else {
+  $( "#dropdown_content_4" ).fadeOut("slow");
+  $("#dropdown_batton_4").toggleClass("screen_9_dropdown_batton_pointer_spin");
+  }
+});
